cc9e79bbe0b5a21d0a08fd8d937d6c62c0041b6c
ritikakathare <ritikakathare15@gmail.com>
2025-10-30 16:12:53 +0530

    Create teach2.py

---
+++ teach2.py
@@ -0,0 +1,7 @@
+teacher_name=input("enter the teacher name")
+subject=input("enter the subject")
+course_code=(int(input("Enter the course code"))
+print("teacher name:",teacher_name)
+print("Subject:",subject)
+print("course code:",course_code)
+             
